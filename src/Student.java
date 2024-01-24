92c7174c9422cad0d0a01882d29ad4ea6309f9d0
Tejas Lale <tejaslale29@gmail.com>
2024-01-24 18:13:18 +0530

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    int rollno;
+    String StudentName;
+
+    public static void main(String[] args) {
+        Student ogj1 = new Student();
+        System.out.println(ogj1.rollno);
+        System.out.println(ogj1.StudentName);
+    }
+}
